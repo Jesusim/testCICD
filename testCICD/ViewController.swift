9571d043b9d7efa4bfd6d6756da79a5f1b600087
Kolesnikov <jesusimtoday@gmail.com>
2022-04-20 13:43:09 +0700

    set backgroundColor

--- testCICD/ViewController.swift
+++ testCICD/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let mmm = "HI"
+        print(mmm)
+        view.backgroundColor = .red
     }
 
 
