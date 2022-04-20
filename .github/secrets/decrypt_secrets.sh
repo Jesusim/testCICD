aa265ddb2db48a397aced585e4e03b289bb40a88
DK <jesusimtoday@gmail.com>
2022-04-20 17:12:45 +0700

    Update decrypt_secrets.sh

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -1,7 +1,5 @@
 set -eo pipefail
 
-git update-index --chmod=+x ./.github/scripts/backend_decrypt.sh
-
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/match_AppStore_comtiagosantocalculatorIOS.mobileprovision ./.github/secrets/match_AppStore_comtiagosantocalculatorIOS.mobileprovision.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
 
