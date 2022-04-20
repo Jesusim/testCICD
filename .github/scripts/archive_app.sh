a8ea3e4b154d87e432176360dcc75a8d618d2e02
DK <jesusimtoday@gmail.com>
2022-04-20 18:12:42 +0700

    Update archive_app.sh

--- .github/scripts/archive_app.sh
+++ .github/scripts/archive_app.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 xcodebuild -workspace testCICD.xcworkspace \
             -scheme testCICD
-            #-sdk iphoneos \
-            #-configuration AppStoreDistribution \
+            -sdk iphoneos \
+            -configuration AppStoreDistribution \
             -archivePath $PWD/build/testCICD.xcarchive \
             clean archive | xcpretty
