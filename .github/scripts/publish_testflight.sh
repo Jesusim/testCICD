c297486f1850fa93f0c48da5566235708bbf1d25
DK <jesusimtoday@gmail.com>
2022-04-21 14:27:11 +0700

    Update publish_testflight.sh

--- .github/scripts/publish_testflight.sh
+++ .github/scripts/publish_testflight.sh
@@ -1,3 +1,3 @@
 set -eo pipefail
 
-xcrun altool --upload-app -t ios -f build/testCICD/iOS.ipa -u "$APPLEID_USERNAME" -p "$APPLEID_PASSWORD" --verbose
+xcrun altool --upload-app -t ios -f build/testCICD.ipa -u "$APPLEID_USERNAME" -p "$APPLEID_PASSWORD" --verbose
